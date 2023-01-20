60dd80af4e587beae8dd8e12737767f05780cd37
Seth Rogers <Seth.Rogers7420@gmail.com>
2023-01-20 12:45:24 -0500

    Add husky

--- src/mappers/buildTypeMappers.ts
+++ src/mappers/buildTypeMappers.ts
@@ -1,11 +1,10 @@
-import { readFileSync } from "fs";
+import { readFileSync } from 'fs';
 
-
-export function buildTypeMappers( pathToMap: string | string[],
-  mapperName = "GraphQLTypes"): Record<string, string> {
-
+export function buildTypeMappers(pathToMap: string | string[], mapperName = 'GraphQLTypes'): Record<string, string> {
   if (!pathToMap || pathToMap.length === 0) {
-    throw new Error("Please enter one or more paths to files containing your TypeScript mappers declaration. See documentation for details.");
+    throw new Error(
+      'Please enter one or more paths to files containing your TypeScript mappers declaration. See documentation for details.'
+    );
   }
 
   if (!Array.isArray(pathToMap)) {
@@ -14,12 +13,9 @@ export function buildTypeMappers( pathToMap: string | string[],
 
   let mappers: Record<string, string> = {};
 
-
   for (const path of pathToMap) {
-    mappers = {...createMappersFromFile(path, mapperName)}
+    mappers = { ...createMappersFromFile(path, mapperName) };
   }
 }
 
-function createMappersFromFile(filePath: string, mapperName: string): Record<string, string> {
-
-}
+function createMappersFromFile(filePath: string, mapperName: string): Record<string, string> {}
