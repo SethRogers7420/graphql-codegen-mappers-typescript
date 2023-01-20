8d916c4f49d621f74d0de4c298cae62cf40c5238
Seth Rogers <Seth.Rogers7420@gmail.com>
2023-01-20 12:28:59 -0500

    Initial commit

---
+++ src/mappers/buildTypeMappers.ts
@@ -0,0 +1,26 @@
+import { readFileSync } from "fs";
+
+
+export function buildTypeMappers( pathToMap: string | string[],
+  mapperName = "GraphQLTypes"): Record<string, string> {
+  let { pathToMap, mapperName = "GraphQLTypes" } = options;
+
+  if (!pathToMap || pathToMap.length === 0) {
+    throw new Error("Please enter one or more paths to files containing your TypeScript mappers declaration. See documentation for details.");
+  }
+
+  if (!Array.isArray(pathToMap)) {
+    pathToMap = [pathToMap];
+  }
+
+  let mappers: Record<string, string> = {};
+
+
+  for (const path of pathToMap) {
+    mappers = {...createMappersFromFile(path, mapperName)}
+  }
+}
+
+function createMappersFromFile(filePath: string, mapperName: string): Record<string, string> {
+
+}
